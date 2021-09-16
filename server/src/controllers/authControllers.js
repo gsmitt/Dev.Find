59d9242ef8840f1d4468f8f34af60e539c6b893b
Gustavo <gundelhausmann@gmail.com>
2021-09-16 17:06:18 -0300

    trabalhando nos tokens

--- server/src/controllers/authControllers.js
+++ server/src/controllers/authControllers.js
@@ -4,6 +4,10 @@ const jwt = require("jsonwebtoken");
 const validator = require("email-validator");
  
 
+function createAcessToken(id) {
+
+}
+
 async function login(req, res, next) {
     try {
         const { credential, password } = req.body;
@@ -15,8 +19,18 @@ async function login(req, res, next) {
         } else {
             registeredUser = await User.findOne({ where: { username: credential } });
         }
-            
-        res.json(registeredUser);
+
+        if (!registeredUser) {
+            throw new createHttpError(401, "Invalid credentials");
+        }
+        
+        const isPasswordValid = registeredUser.isPasswordValid(password);
+
+        if (!isPasswordValid) {
+            throw new createHttpError(401, "Invalid credentials");
+        }
+
+        res.json("Arrumar depois");
 
     } catch (error) {
         console.log(error);
