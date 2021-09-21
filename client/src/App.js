a36516cdd49a14dc7173f76a52a4afaf80499703
João Victor <joao.ervilhojv@gmail.com>
2021-09-21 15:25:13 -0300

    front-end #9

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import React from "react";
 import { NavBar } from "../src/components/Navbar";
+import { Footer } from "../src/components/Footer";
 import { Home } from "../src/pages/Home"
+
 
 function App() {
   return (
     <div className="App">
       <NavBar></NavBar>
       <Home></Home>
+      <Footer></Footer>
+
     </div>
   );
 }
