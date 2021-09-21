e14b736e37074bfcacbff27abdba68d53093c387
Jessé Antônio <jesseantonio1321@gmail.com>
2021-09-21 17:11:07 -0300

    front-end #12

---
+++ client/src/components/Navbar/SidebarData.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import * as IoIcons from 'react-icons/io';
+
+export const SidebarData = [
+  {
+    title: 'Home',
+    path: '/',
+    cName: 'nav-text'
+  },
+  {
+    title: 'Sobre',
+    path: '/sobre',
+    cName: 'nav-text'
+  },
+  {
+    title: 'Explorar',
+    path: '/explorar',
+    cName: 'nav-text'
+  },
+  {
+    title: 'Cadastrar-se',
+    path: '/cadastro',
+    cName: 'nav-text'
+  },
+  {
+    title: 'Contato',
+    path: '/contato',
+    cName: 'nav-text'
+  },
+];
