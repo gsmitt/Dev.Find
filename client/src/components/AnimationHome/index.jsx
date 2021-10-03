65bcc3ff0da9453ab456bb01f1e40de221b928ee
Gustavo <gundelhausmann@gmail.com>
2021-10-03 09:43:20 -0300

    corrigindo a animação

--- client/src/components/AnimationHome/index.jsx
+++ client/src/components/AnimationHome/index.jsx
@@ -1,25 +1,78 @@
 import "./styles.css";
-import useEffect from "react"
-const anime = require('animejs');
+import { useEffect } from 'react';
+import anime from 'animejs/lib/anime.es'
 
 export function AnimationHome(props) {
-
     useEffect(
-        anime({
-            targets: ".dot",
-            translateY: [1000,-100],
-            //borderRadius: 50,
-            duration: 3000,
-            direction: "normal",
-            loop: true,
-            //rotate: 90,
-            easing: "linear"
-        })
+        () => {
+            const container = document.querySelector(".cont");
+
+            var l = 150;
+        
+            for (var i = 0; i <= l; i += 1) {
+                var x = anime.random(0, 100);
+        
+                var n = 1;
+        
+                for (var j = 0; j < n; j++) {
+                    var dot = document.createElement("div");
+                    dot.classList.add("dot");
+                    container.appendChild(dot);
+                    console.log(document)
+                    if (i <= 75) {
+                        dot.classList.add("dot1");
+                    } else {
+                        dot.classList.add("dot2");
+                    }
+        
+                    var size = anime.random(2, 10);
+        
+                    dot.style.width = size + "px";
+                    dot.style.height = size + "px";
+        
+                    dot.style.left = x + "vw";
+                    dot.style.top = "100vh";
+        
+        
+                }
+            }
+            anime({
+                loop: true,
+                easing: "linear",
+                targets: document.querySelectorAll(".dot1"),
+                translateY: {
+                  value: "-110vh",
+                  
+                  duration: function(e,i){
+                    return anime.random(20000,25000) / e.style.width.replace("px","")
+                  },
+                  delay: anime.stagger(150)
+                }
+              });
+              
+              
+              anime({
+                loop: true,
+                easing: "linear",
+                targets: document.querySelectorAll(".dot2"),
+                translateY: {
+                  value: "-110vh",
+                  
+                  duration: function(e,i){
+                    return anime.random(40000,50000) / e.style.width.replace("px","")
+                  },
+                  delay: function(e,i){
+                    return (i-1) * 150 + 11000
+                  }
+                  
+                }
+              });
+
+        }
     )
 
     return (
-        <div className="container">
-            <div className="dot"></div>
+        <div className="cont">
         </div>
     );
 }
