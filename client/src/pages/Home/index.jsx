206dd73cac2b2ef4ffbf8f209ed33923c22e8605
Gustavo <gundelhausmann@gmail.com>
2021-10-05 17:19:36 -0300

    correção de bugs

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -48,9 +48,7 @@ export class Home extends React.Component {
                             <a href="/publicar-projeto"><button className="botao-contratar"><p className="texto-botao-contratar">Contratar</p></button></a>
                             <br />
                             <a href="/login"><button className="botao-seja-um"><p className="texto-botao-seja-um">Seja um desenvolvedor</p></button></a>
-
                         </center>
-
                     </div>
                 </div>
                 <div className="desc">
