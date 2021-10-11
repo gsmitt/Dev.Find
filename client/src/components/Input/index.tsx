fcc7a93ad2df423f2792fdb447a3e28eae40ad05
João Victor <joao.ervilhojv@gmail.com>
2021-10-11 17:21:08 -0300

    front-end #45 | fix input profile

---
+++ client/src/components/Input/index.tsx
@@ -0,0 +1,17 @@
+import React, { FC, InputHTMLAttributes } from 'react';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  label: string;
+}
+
+const Input: FC<InputProps> = ({ name, label, ...rest }) => {
+  return (
+    <div className="input-profile">
+      <label htmlFor={name}>{label}</label>
+      <input id={name} { ...rest }></input>
+    </div>
+  );
+};
+
+export default Input;
