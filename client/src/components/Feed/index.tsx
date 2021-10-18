58db3013ba3c29895ef2d15a7c2ce9cd4daca11f
Gustavo <gundelhausmann@gmail.com>
2021-10-17 23:25:18 -0300

    trabalhando no perlfil

--- client/src/components/Feed/index.tsx
+++ client/src/components/Feed/index.tsx
@@ -1,44 +1,72 @@
 import React, { useState, useEffect } from "react";
 import { api } from "../../services/api";
+import './styles.css';
 
 import Post from '../Posts';
 
 import { Tab_Container, Container_Feed, Tab, Posts } from './styles';
 
 const Feed: React.FC = () => {
-  /*
   let [data, setData] = useState({
-    offset: 0
+    offset: 0,
+    posts: [{
+      title: "",
+      image: "",
+      description: "",
+      updatedAt: ""
+    }],
+    selected: "post"
   });
 
   useEffect(() => {
-
-    async function load() {
-      const id = window.location.pathname.slice(16)
-      try {
-        const get = (await api.get(`/post/getByUser/${id}/${data.offset}`)).data;
-
-      } catch (err) {
-        console.log(err);
-      }
-    }
-
-    load()
-    return () => {
-      cancelTokenSource.cancel();
-    }
+    load(data.selected)
   }, []);
-  */
+
+  async function load(type) {
+    const id = window.location.pathname.slice(16)
+    try {
+      const get = (await api.get(`/${type}/getByUser/${id}/${data.offset}`)).data;
+      setData(prevData => ({ ...prevData, posts: get }));
+      console.log(get)
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async function handleSelector(e) {
+    const value = e.target.id.slice(1)
+    setData(prevData => ({ ...prevData, selected: value }));
+    load(data.selected)
+  }
+
   return (
     <Container_Feed>
       <Tab_Container>
-        <Tab>Projetos</Tab>
-        <Tab>Reviews</Tab>
+        <Tab className={data.selected == "post" ? "selected-tab" : ""} id="-post" onClick={handleSelector}><p>Projetos</p></Tab>
+        <Tab className={data.selected == "review" ? "selected-tab" : ""} id="-review" onClick={handleSelector}><p>Reviews</p></Tab>
       </Tab_Container>
       <Posts className="post-container">
-        <Post />
-        <Post />
-        <Post />
+        {data.selected == "post"? (data.posts).map((item, index) => {
+          return (
+            <Post
+              title={item.title}
+              date={item.updatedAt}
+              image={item.image}
+              description={item.description}
+              num={index} />
+          );
+        }):
+
+        (data.posts).map((item, index) => {
+          return (
+            <Post
+              title={item.title}
+              date={item.updatedAt}
+              image={item.image}
+              description={item.description}
+              num={index} />
+          );
+        })}
       </Posts>
     </Container_Feed>
   );
--- server/src/routes/reviewRoutes.js
+++ server/src/routes/reviewRoutes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const reviewControllers = require("../controllers/reviewControllers");
 const auth = require("../middlewares/authMiddleware");
 
-router.get("/:id", auth(["admin","dev","client"]), reviewControllers.get);
+router.get("/getByUser/:id", auth(["admin","dev","client"]), reviewControllers.get);
 router.post("/", auth(["admin","dev","client"]), reviewControllers.create);
 router.delete("/:id", auth(["admin","dev","client"]), reviewControllers.deleteReview);
 router.put("/:id", auth(["admin","dev","client"]), reviewControllers.update);
